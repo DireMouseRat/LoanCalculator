405449161393994808577280e07e140d1f317c2d
Joshua Meis <67470381+DireMouseRat@users.noreply.github.com>
2021-11-17 20:06:28 -0600

    Unmerged changes

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,45 +1,52 @@
 import math
+import argparse
 
 
-def date_type_to_str(date_type, date_count):
-    if date_count == 0:
+def pluralize(word, quantity):
+    if quantity == 0:
         return str()
-    plural = str() if date_count == 1 else 's'
-    return f'{date_count} {date_type}{plural}'
+    plural = str() if quantity == 1 else 's'
+    return f'{quantity} {word}{plural}'
 
 
 class LoanCalculator:
 
     def __init__(self):
-        self.principal = 0
-        self.payment = 0
-        self.payments_number = 0
+        self.loan_principal = 0
+        self.annuity_payment = 0
+        self.differentiated_payment = 0
+        self.number_of_payments = 0
         self.nominal_interest_rate = 0
+        self.annual_interest_rate = 0
+        self.current_repayment_month = 0
 
     def get_inputs(self, principal, payment, periods, interest):
-        self.principal = int(input("Enter the loan principal:\n")) if principal else 0
-        self.payment = float(input("Enter the monthly payment:\n")) if payment else 0
-        self.payments_number = int(input("Enter the number of periods:\n")) if periods else 0
+        self.loan_principal = int(input("Enter the loan principal:\n")) if principal else 0
+        self.annuity_payment = float(input("Enter the monthly payment:\n")) if payment else 0
+        self.number_of_payments = int(input("Enter the number of periods:\n")) if periods else 0
         self.nominal_interest_rate = float(input("Enter the loan interest:\n")) / 1200 if interest else 0
 
     def get_principal(self):
-        p = self.principal
-        a = self.payment
-        n = self.payments_number
+        p = self.loan_principal
+        a = self.annuity_payment
+        d = self.differentiated_payment
+        n = self.number_of_payments
         i = self.nominal_interest_rate
         return int(a / ((i * ((1 + i) ** n)) / (((1 + i) ** n) - 1)))
 
     def get_payment(self):
-        p = self.principal
-        a = self.payment
-        n = self.payments_number
+        p = self.loan_principal
+        a = self.annuity_payment
+        d = self.differentiated_payment
+        n = self.number_of_payments
         i = self.nominal_interest_rate
         return math.ceil(p * (i * ((1 + i) ** n)) / (((1 + i) ** n) - 1))
 
     def get_periods(self):
-        p = self.principal
-        a = self.payment
-        n = self.payments_number
+        p = self.loan_principal
+        a = self.annuity_payment
+        d = self.differentiated_payment
+        n = self.number_of_payments
         i = self.nominal_interest_rate
         return math.ceil(math.log(a / (a - i * p), 1 + i))
 
@@ -51,6 +58,15 @@ class LoanCalculator:
     #     return 0.10 / 12
 
 
+error_message = "Incorrect parameters"
+parser = argparse.ArgumentParser()
+parser.add_argument("--type", choices=["annuity", "diff"], required=True)
+parser.add_argument("--payment")
+parser.add_argument("--principal")
+parser.add_argument("--periods")
+parser.add_argument("--interest")
+
+args = parser.parse_args()
 LC = LoanCalculator()
 menu = input("""What do you want to calculate?
 type "n" for number of monthly payments,
@@ -59,17 +75,15 @@ type "p" for loan principal:\n""")
 
 if menu == 'p':
     LC.get_inputs(False, True, True, True)
-    principal = LC.get_principal()
-    print(f"Your loan principal = {principal}!")
+    print(f"Your loan principal = {LC.get_principal()}!")
 elif menu == 'a':
     LC.get_inputs(True, False, True, True)
-    payment = LC.get_payment()
-    print(f"Your monthly payment = {payment}!")
+    print(f"Your monthly payment = {LC.get_payment()}!")
 if menu == 'n':
     LC.get_inputs(True, True, False, True)
     periods = LC.get_periods()
-    years = date_type_to_str('year', math.floor(periods / 12))
-    months = date_type_to_str('month', periods % 12)
+    years = pluralize('year', math.floor(periods / 12))
+    months = pluralize('month', periods % 12)
     and_str = ' and ' if years and months else ''
     print(f"It will take {years + and_str + months} to repay this loan!")
 
