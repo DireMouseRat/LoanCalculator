61606de46ed71586aeed0671e57e29b6173f5a4b
Joshua Meis <67470381+DireMouseRat@users.noreply.github.com>
2021-11-17 21:04:15 -0600

    Added differentiated payment equation. Removed get_input in favor of setting everything in the constructor.

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -11,25 +11,16 @@ def pluralize(word, quantity):
 
 class LoanCalculator:
 
-    def __init__(self):
-        self.loan_principal = 0
-        self.annuity_payment = 0
-        self.differentiated_payment = 0
-        self.number_of_payments = 0
-        self.nominal_interest_rate = 0
-        self.annual_interest_rate = 0
-        self.current_repayment_month = 0
-
-    def get_inputs(self, principal, payment, periods, interest):
-        self.loan_principal = int(input("Enter the loan principal:\n")) if principal else 0
-        self.annuity_payment = float(input("Enter the monthly payment:\n")) if payment else 0
-        self.number_of_payments = int(input("Enter the number of periods:\n")) if periods else 0
-        self.nominal_interest_rate = float(input("Enter the loan interest:\n")) / 1200 if interest else 0
+    def __init__(self, loan_type, principal, payment, periods, interest):
+        self.loan_type = loan_type
+        self.loan_principal = int(principal)
+        self.annuity_payment = float(payment) if payment is not None else 0
+        self.number_of_payments = int(periods)
+        self.nominal_interest_rate = float(interest) / 1200
 
     def get_principal(self):
         p = self.loan_principal
         a = self.annuity_payment
-        d = self.differentiated_payment
         n = self.number_of_payments
         i = self.nominal_interest_rate
         return int(a / ((i * ((1 + i) ** n)) / (((1 + i) ** n) - 1)))
@@ -37,25 +28,23 @@ class LoanCalculator:
     def get_payment(self):
         p = self.loan_principal
         a = self.annuity_payment
-        d = self.differentiated_payment
         n = self.number_of_payments
         i = self.nominal_interest_rate
         return math.ceil(p * (i * ((1 + i) ** n)) / (((1 + i) ** n) - 1))
 
+    def get_differentiated_payments(self):
+        p = self.loan_principal
+        a = self.annuity_payment
+        n = self.number_of_payments
+        i = self.nominal_interest_rate
+        return [math.ceil((p / n) + i * (p - ((p * (m - 1)) / n))) for m in range(1, n + 1)]
+
     def get_periods(self):
         p = self.loan_principal
         a = self.annuity_payment
-        d = self.differentiated_payment
         n = self.number_of_payments
         i = self.nominal_interest_rate
         return math.ceil(math.log(a / (a - i * p), 1 + i))
-
-    # def get_interest(self):
-    #     p = self.principal
-    #     a = self.payment
-    #     n = self.periods
-    #     i = self.interest
-    #     return 0.10 / 12
 
 
 error_message = "Incorrect parameters"
@@ -65,27 +54,29 @@ parser.add_argument("--payment")
 parser.add_argument("--principal")
 parser.add_argument("--periods")
 parser.add_argument("--interest")
+parser.error(error_message)
+
 
 args = parser.parse_args()
-LC = LoanCalculator()
-menu = input("""What do you want to calculate?
-type "n" for number of monthly payments,
-type "a" for annuity monthly payment amount,
-type "p" for loan principal:\n""")
 
-if menu == 'p':
-    LC.get_inputs(False, True, True, True)
-    print(f"Your loan principal = {LC.get_principal()}!")
-elif menu == 'a':
-    LC.get_inputs(True, False, True, True)
-    print(f"Your monthly payment = {LC.get_payment()}!")
-if menu == 'n':
-    LC.get_inputs(True, True, False, True)
-    periods = LC.get_periods()
-    years = pluralize('year', math.floor(periods / 12))
-    months = pluralize('month', periods % 12)
-    and_str = ' and ' if years and months else ''
-    print(f"It will take {years + and_str + months} to repay this loan!")
+
+LC = LoanCalculator(args.type, args.principal, args.payment, args.periods, args.interest)
+
+print(LC.get_differentiated_payments())
+
+# if menu == 'p':
+#     LC.get_inputs(False, True, True, True)
+#     print(f"Your loan principal = {LC.get_principal()}!")
+# elif menu == 'a':
+#     LC.get_inputs(True, False, True, True)
+#     print(f"Your monthly payment = {LC.get_payment()}!")
+# if menu == 'n':
+#     LC.get_inputs(True, True, False, True)
+#     periods = LC.get_periods()
+#     years = pluralize('year', math.floor(periods / 12))
+#     months = pluralize('month', periods % 12)
+#     and_str = ' and ' if years and months else ''
+#     print(f"It will take {years + and_str + months} to repay this loan!")
 
     # payment = math.ceil(principal / months)
     # if principal % months == 0:
